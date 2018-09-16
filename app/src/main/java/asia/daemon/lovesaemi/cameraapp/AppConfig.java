81b87db7562414fe76c40484568da8b4bd02648b
modeverv <modeverv@gmail.com>
2018-09-16 15:09:46 +0900

    initial

---
+++ app/src/main/java/asia/daemon/lovesaemi/cameraapp/AppConfig.java
@@ -0,0 +1,10 @@
+package asia.daemon.lovesaemi.cameraapp;
+
+/**
+ * Created by seijiro on 2018/09/16.
+ */
+public class AppConfig {
+    public static final String STREAM_URL = "rtsp://192.168.111.8:8997/live/lovesaemi";
+    public static final String PUBLISHER_USERNAME = "lovesaemi";
+    public static final String PUBLISHER_PASSWORD = "password";
+}
